bb1c81c639b5621c5dbc24f5b491ba91bc688c82
bsavci22 <bsavci@sabanciuniv.edu>
2021-11-12 14:40:44 +0300

    berkay register
    ->Register added

---
+++ unigov1/events/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Usersv2
+
+# Register your models here
+admin.site.register(Usersv2)
---
+++ unigov1/events/urls.py
@@ -0,0 +1,17 @@
+
+from django import urls
+from django.conf.urls import url
+from django.urls import path,include
+from . import views
+
+
+urlpatterns = [
+    #path('',views.hometester,name="home"),
+    path('loginstudent/',views.home,name="userlogin"),
+    path('loginstudent/checkuser',views.checkuser,name="checkuser"),
+    path('register/',views.register,name="register"),
+    path('register/adduser',views.adduser,name="register"),
+    
+
+    
+]
